f16ab3c9f8c07447122253b62b8bf869f0465366
Vacuium <dwjmath5@gmail.com>
2023-07-28 09:06:29 +0800

    bug fixed

--- vecdbsetting.py
+++ vecdbsetting.py
@@ -103,7 +103,7 @@ for file_name in files_name:
     text = read_and_clean_pdf_text(file_path)
     
     # Chunk each document, embed the contents and load to Redis
-    handle_file_string((file_name,text[0].decode("utf-8")),tokenizer,redis_client,VECTOR_FIELD_NAME,INDEX_NAME)
+    handle_file_string((file_name,text[0]),tokenizer,redis_client,VECTOR_FIELD_NAME,INDEX_NAME)
 
 # Check that our docs have been inserted
 logging.info(redis_client.ft(INDEX_NAME).info()['num_docs'])
