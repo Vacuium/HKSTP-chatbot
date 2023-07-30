ff18f02e91f0ade5937705185856000ed2bfe207
Vacuium <dwjmath5@gmail.com>
2023-07-31 00:15:22 +0800

    upload building

--- hkstp_chatbot/app.py
+++ hkstp_chatbot/app.py
@@ -1,12 +1,23 @@
-from flask import Flask, request, jsonify, render_template, session
+from flask import Flask, request, jsonify, render_template, session, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
 # from database import get_redis_connection
 # from chatbot import RetrievalAssistant, Message, IncubationAgent
+
+
+UPLOAD_FOLDER = r'.\UPLOAD_FILE'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
 
 class Config(object):
     SECRET_KEY = "DJFAJLAJAFKLJQ"
+    UPLOAD_FOLDER = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -31,6 +42,23 @@ def submit():
 
     # return jsonify({'response': response})
     return text
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        logging.info(type(file))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully'}), 200
 
 if __name__ == '__main__':
     app.config.from_object(Config())
